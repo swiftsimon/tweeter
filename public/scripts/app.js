5f61bdcb7c6055dc8458007556f0a4ac8951be44
Simon <simon@simonhamilton.ca>
2018-01-30 19:28:37 +0000

    character counter plus red text class

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+console.log("app page");
+
+ $(document).ready(function() {
+  console.log("doc ready");
+});
+
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,25 @@
+
+$(document).ready(function() {
+  $(".new-tweet > textarea").keyup(function() {    // target textarea tag from index.html
+    var charLeft = (140 - $(this).val().length)  // this refers to "textarea"
+      // console.log($(this).find(".counter"))
+    var targetCounter = $(this).closest(".new-tweet").find(".counter")
+            // traverse up (.closest) to find parent with .new-tweet class
+            //then traveerse down (.find) to find sibing with class .counter
+
+    targetCounter.text(charLeft)  // use .text to update the value of the counter
+
+    if (charLeft < 0) {
+      targetCounter.addClass('redText')
+    } else {
+      targetCounter.removeClass('redText')
+    }
+
+  })
+});
+
+
+   // var charLeft = (140 - (this).val().length)  // this refers to "textarea"
+   //    // console.log($(this).find(".counter"))
+   //  var targetCounter = (this).closest(".new-tweet").find(".counter")
+   //  var targetCounter = (this).closest(".new-tweet").find(".counter")
